ab23b7e3b9ca7d53f9ab40ff037ba785202365a5
Surendra Jat <surendrajat9079@gmail.com>
2024-06-09 13:32:51 +0530

    Changed the user timeline findbyid

--- controllers/tweet.controller.js
+++ controllers/tweet.controller.js
@@ -23,17 +23,55 @@ exports.createNewTweet = async (req, res)=>{
 
 }
 
-exports.findTweetsByUserId = async (req, res)=>{
-    try{
-        const userId = req.body.userId
+// exports.findTweetsByUserId = async (req, res)=>{
+//     try{
+//         const userId = req.body.userId
         
-        const tweets = await tweet_model.find({userId}).populate("userId", "username").sort({createdAT: -1})
+//         const tweets = await tweet_model.find({userId}).populate("userId", "username").sort({createdAT: -1})
+
+//         if (!tweets || tweets.length === 0) {
+//             return res.status(404).send({ message: 'No tweets found for this user' });
+//         }
+//     return res.status(200).send(tweets);
+//     }catch (error) {
+//         console.error('Error while finding tweets by userId:', error);
+//         return res.status(500).send({ message: 'Internal Server Error' });
+//     }
+// }
+
+exports.findTweetsByUserId = async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const { cursor, limit = 10 } = req.query;
+
+        // query
+        let query = { userId };
+        if (cursor) {
+            query._id = { $lt: mongoose.Types.ObjectId(cursor) }; // Fetch tweets with IDs less than the cursor
+        }
+
+        const tweets = await tweet_model.find(query)
+            .populate("userId", "username")
+            .sort({ _id: -1 })
+            .limit(Number(limit) + 1); // Fetch one more tweet to check if there are more results
 
         if (!tweets || tweets.length === 0) {
             return res.status(404).send({ message: 'No tweets found for this user' });
         }
 
-    }catch (error) {
+        const hasNextPage = tweets.length > limit;
+        if (hasNextPage) {
+            tweets.pop(); // Remove the extra tweet used for the check
+        }
+
+        const nextCursor = hasNextPage ? tweets[tweets.length - 1]._id : null;
+
+        return res.status(200).send({
+            tweets,
+            nextCursor,
+            hasNextPage
+        });
+    } catch (error) {
         console.error('Error while finding tweets by userId:', error);
         return res.status(500).json({ message: 'Internal Server Error' });
     }
